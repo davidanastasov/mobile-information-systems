637cdbd4f49d4bf983d46e3ed6aef03354181f01
David Anastasov <davidanastasov@hotmail.com>
2025-01-10 04:16:51 +0100

    Lab 03 Completed

--- mis_lab_03/lib/main.dart
+++ mis_lab_03/lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:timezone/timezone.dart' as tz;
+import 'package:timezone/data/latest_10y.dart' as tz_data;
 import 'package:mis_lab_03/screens/favourite.dart';
 import 'package:mis_lab_03/screens/home.dart';
 import 'package:mis_lab_03/screens/jokes.dart';
@@ -7,11 +10,27 @@ import 'package:mis_lab_03/screens/random_joke.dart';
 
 import 'firebase_options.dart';
 
+final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+FlutterLocalNotificationsPlugin();
+
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+
+  tz_data.initializeTimeZones();
+
+  // Local notifications
+  const AndroidInitializationSettings initializationSettingsAndroid =
+  AndroidInitializationSettings('@mipmap/ic_launcher');
+  const InitializationSettings initializationSettings =
+  InitializationSettings(android: initializationSettingsAndroid);
+
+  await flutterLocalNotificationsPlugin.initialize(initializationSettings);
+
+  scheduleDailyNotification();
+
   runApp(const MyApp());
 }
 
@@ -36,4 +55,33 @@ class MyApp extends StatelessWidget {
       },
     );
   }
+}
+
+Future<void> scheduleDailyNotification() async {
+  await flutterLocalNotificationsPlugin.zonedSchedule(
+    0,
+    'Joke of the Day',
+    'There is a new joke of the day, have a look!',
+    _nextInstanceOfTime(10, 0), // Every day at 10AM
+    const NotificationDetails(
+        android: AndroidNotificationDetails(
+            'daily_channel', 'Daily Reminders',
+            importance: Importance.high)),
+    uiLocalNotificationDateInterpretation:
+        UILocalNotificationDateInterpretation.absoluteTime,
+    matchDateTimeComponents: DateTimeComponents.time,
+    androidScheduleMode: AndroidScheduleMode.inexact,
+  );
+}
+
+tz.TZDateTime _nextInstanceOfTime(int hour, int minute) {
+  final tz.TZDateTime now = tz.TZDateTime.now(tz.local);
+  tz.TZDateTime scheduledDate =
+      tz.TZDateTime(tz.local, now.year, now.month, now.day, hour, minute);
+
+  if (scheduledDate.isBefore(now)) {
+    scheduledDate = scheduledDate.add(const Duration(days: 1));
+  }
+
+  return scheduledDate;
 }
