a45aec09ff660a79618a24e8c69e41352c5b3b88
David Anastasov <davidanastasov@hotmail.com>
2025-01-10 02:54:45 +0100

    Configure firebase

--- mis_lab_03/lib/main.dart
+++ mis_lab_03/lib/main.dart
@@ -1,10 +1,17 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:mis_lab_03/screens/favourite.dart';
 import 'package:mis_lab_03/screens/home.dart';
 import 'package:mis_lab_03/screens/jokes.dart';
 import 'package:mis_lab_03/screens/random_joke.dart';
 
-void main() {
+import 'firebase_options.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
